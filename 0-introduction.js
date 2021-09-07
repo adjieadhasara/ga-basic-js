7dfd4675434c7430b040fb31e5a22ab9518593ba
rnwathon <rakhmatnashrul@gmail.com>
2021-09-07 21:54:58 +0800

    initial commit introduction

---
+++ 0-introduction.js
@@ -0,0 +1,41 @@
+/*
+
+  Key JavaScript Programming Concepts:
+  
+  - Syntax & Semantic
+    - Syntax adalah aturan penulisan kode program.
+      Ketika adalah aturan penulisan yang dilanggar, kita akan dapet Syntax Error.
+      
+      Contoh dalam analogi Bahasa Indonesia:
+      "Budi pergi ke pasar" -> Syntax Benar
+      "pergi ke Budi pasar" -> Syntax Error
+      "perg k Bud pasar" -> Syntax Error
+
+    - Semantic mengacu ke makna dari apa yang tulis.
+      Apa yang kita tulis, bisa jadi secara Syntax tapi secara makna gak bisa dicerna.
+      Ketika semantic error terjadi, program akan tetep jalan tapi hasilnya ga akan sesuai
+      dengan apa yang kita mau.
+
+      Contoh dalam analogi Bahasa Indonesia:
+      "Botol tipes" -> doesn't make sense
+
+  - Reserve Keywords / symbols
+
+    var, let, const, function, class, typeof dst.
+
+
+  - Variable & Data Types
+  - Operator
+  - Function
+  - Scope
+  - Conditionals
+  - Looping
+  - Array
+    - How to traverse / getting the data
+    - How to manipulate
+  - Object
+    - How to traverse / getting the data
+    - How to manipulate
+
+
+*/
